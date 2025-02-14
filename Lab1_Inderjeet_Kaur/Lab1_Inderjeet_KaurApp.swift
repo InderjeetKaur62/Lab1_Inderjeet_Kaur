6edd411dbd9d542280e445e2b144906e9cac4553
InderjeetKaur62 <101415953@georgebrown.ca>
2025-02-13 20:58:16 -0500

    Initial Commit

---
+++ Lab1_Inderjeet_Kaur/Lab1_Inderjeet_KaurApp.swift
@@ -0,0 +1,17 @@
+//
+//  Lab1_Inderjeet_KaurApp.swift
+//  Lab1_Inderjeet_Kaur
+//
+//  Created by Inderjeet Kaur on 2025-02-13.
+//
+
+import SwiftUI
+
+@main
+struct Lab1_Inderjeet_KaurApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Lab1_Inderjeet_KaurTests/Lab1_Inderjeet_KaurTests.swift
@@ -0,0 +1,17 @@
+//
+//  Lab1_Inderjeet_KaurTests.swift
+//  Lab1_Inderjeet_KaurTests
+//
+//  Created by Inderjeet Kaur on 2025-02-13.
+//
+
+import Testing
+@testable import Lab1_Inderjeet_Kaur
+
+struct Lab1_Inderjeet_KaurTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
